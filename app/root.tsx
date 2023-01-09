13c37ab9cee76f747bdfea070e97c7418ad37cd7
xHomu <84349818+xHomu@users.noreply.github.com>
2023-01-09 14:32:59 -0800

    Initial commit

---
+++ app/root.tsx
@@ -0,0 +1,53 @@
+import type { LoaderFunction, MetaFunction } from "@remix-run/node";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import { rootAuthLoader } from "@clerk/remix/ssr.server";
+import { ClerkApp, ClerkCatchBoundary } from "@clerk/remix";
+import styles from "~/styles/shared.css";
+import Header from "~/components/Header";
+
+export const meta: MetaFunction = () => {
+  return { title: "New Remix App" };
+};
+
+export function links() {
+  return [
+    { rel: "stylesheet", href: "https://unpkg.com/modern-css-reset@1.4.0/dist/reset.min.css" },
+    { rel: "stylesheet", href: styles },
+  ];
+}
+
+export const loader: LoaderFunction = (args) => {
+  return rootAuthLoader(
+    args,
+    ({ request }) => {
+      const { userId, sessionId, getToken } = request.auth;
+      console.log("Root loader auth:", { userId, sessionId, getToken });
+      return { message: `Hello from the root loader :)` };
+    },
+    { loadUser: true }
+  );
+};
+
+function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Header />
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export default ClerkApp(App);
+
+export const CatchBoundary = ClerkCatchBoundary();
